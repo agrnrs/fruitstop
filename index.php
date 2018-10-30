e5d0f0491e6665ed18fdc7ece29385066b9fb12d
agrnrs <43164370+agrnrs@users.noreply.github.com>
2018-10-30 10:12:25 +0200

    init

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+ 
+<head>
+<meta charset="utf-8">
+<title>Fruit-stop Ab</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<style>
+
+</style>
+</head>
+
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<ul class="nav">
+ <a class="navbar-brand" href="#">Fruit-stop Ab</a>
+  <li class="nav-item">
+    <a class="nav-link active" href="#">All products</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Type1</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Fruit</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Stuf</a>
+  </li>
+</ul>
+</nav>
+
+<div class="h2 text-center">Products</div>
+
+
+<!-- 
+content
+needs a header for content.
+
+divide list into grid divs somehow
+https://getbootstrap.com/docs/4.0/layout/grid/
+-->
+
+<div class="d-flex p-2">
+	<ul class="list-group">
+		<li class="list-group-item">Cras justo odio</li>
+		<li class="list-group-item">Dapibus ac facilisis in</li>
+		<li class="list-group-item">Morbi leo risus</li>
+		<li class="list-group-item">Porta ac consectetur ac</li>
+		<li class="list-group-item">Vestibulum at eros</li>
+	</ul>
+</div>
+
+
+
+
+<div class="d-flex p-2 fixed-bottom align-items-end d-flex justify-content-end">
+  <div class="col-4 alert alert-secondary text-right ">Login link</div>
+</div>
+
+</body>
---
+++ plan.js
@@ -0,0 +1,36 @@
+/*
+
+tables:
+
+types table:
+id -int2 ai !null, type -varchar/text
+type: fruit, vegetable, berry, other
+
+product table:
+id -int4 ai !null
+product -varchar/text
+type (via types table) -int2
+price -numeric w 2dec
+
+
+//////////////////////////
+
+pages:
+
+index/products
+	-nav top (prob home, types sorts)
+	-product list sort type->alphdesc
+	-login page link
+login
+	-login stuf
+	-notification of status
+edit *add/upd/del products +display relevant +list types
+	-product list, select product by id num, action buttons
+	-logout button
+db_connect
+	-to new db
+ajax?
+
+
+
+*/
