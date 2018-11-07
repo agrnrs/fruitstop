031ce3c43cb4194c19aa0810cb10778a3cf407bb
agrnrs <43164370+agrnrs@users.noreply.github.com>
2018-11-07 11:16:47 +0200

    starting session functionality

--- inc/footer.php
+++ inc/footer.php
@@ -1,11 +1,21 @@
   <!-- v to footer.php -->
-<div class="d-flex p-2 fixed-bottom align-items-end d-flex justify-content-end">
-  <div class="col-4 alert alert-secondary text-center ">Login&nbsp;link</div>
+  
+<?php
+
+if(!isset($_SESSION['login'])){
+//add login button, link to index which is (will be) also login
+?><div class="d-flex p-2 fixed-bottom align-items-end d-flex justify-content-end">
+  <div class="col-4 alert alert-secondary text-center">Login</div>
+</div> <?php
+} else {
+	//have logout button that does this: session_destroy(); 
+	?>
+	<div class="d-flex p-2 fixed-bottom align-items-end d-flex justify-content-end">
+  <div class="col-4 alert alert-secondary text-center">Logout</div>
 </div>
-<!--
+<?php
+};
 
-make login functionality
-make login switch to logout when logged in
+//next: add functionality to login/out button
 
- -->
-
+?>
