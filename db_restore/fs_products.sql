c3e31f0e5ca298dfd37985ed1e59a73701441d57
agrnrs <43164370+agrnrs@users.noreply.github.com>
2018-11-13 22:15:22 +0200

    fs_products now with price and stock

--- db_restore/fs_products.sql
+++ db_restore/fs_products.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2018 at 11:28 AM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 7.2.5
+-- Generation Time: Nov 13, 2018 at 09:14 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,28 +31,36 @@ SET time_zone = "+00:00";
 CREATE TABLE `fs_products` (
   `id` int(8) NOT NULL,
   `product` varchar(64) NOT NULL,
-  `type` int(4) NOT NULL
+  `type` int(4) NOT NULL,
+  `price` decimal(6,3) NOT NULL,
+  `kgstock` int(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `fs_products`
 --
 
-INSERT INTO `fs_products` (`id`, `product`, `type`) VALUES
-(1, 'banana', 1),
-(2, 'Watermelon', 1),
-(3, 'Strawberry', 3),
-(4, 'carrot', 2),
-(5, 'lettuce', 2),
-(6, 'cucumber', 2),
-(7, 'Tomato', 2),
-(9, 'Lime', 1),
-(10, 'Lemon', 1),
-(11, 'paprika', 2),
-(12, 'grape', 1),
-(13, 'brussels sprout', 2),
-(14, 'Apple', 1),
-(15, 'Cloudberry', 3);
+INSERT INTO `fs_products` (`id`, `product`, `type`, `price`, `kgstock`) VALUES
+(1, 'banana', 1, '0.800', 240),
+(2, 'Watermelon', 1, '2.220', 80),
+(3, 'Strawberry', 3, '2.400', 60),
+(4, 'carrot', 2, '0.500', 400),
+(5, 'lettuce', 2, '0.620', 50),
+(6, 'cucumber', 2, '1.350', 80),
+(7, 'Tomato', 2, '0.750', 380),
+(9, 'Lime', 1, '0.999', 120),
+(10, 'Lemon', 1, '0.600', 220),
+(11, 'paprika', 2, '0.999', 130),
+(12, 'grape', 1, '0.950', 40),
+(13, 'brussels sprout', 2, '0.000', 20),
+(14, 'Apple', 1, '0.650', 410),
+(15, 'Cloudberry', 3, '2.850', 45),
+(16, 'sdf', 4, '0.999', 4),
+(17, 'Blank', 2, '0.999', 2),
+(19, 'Ananananas', 1, '0.555', 2),
+(20, 'Nothing', 2, '0.000', 2),
+(21, 'Monkey Brains', 1, '0.999', 2),
+(23, 'One', 1, '0.999', 1);
 
 --
 -- Indexes for dumped tables
@@ -72,7 +80,7 @@ ALTER TABLE `fs_products`
 -- AUTO_INCREMENT for table `fs_products`
 --
 ALTER TABLE `fs_products`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
